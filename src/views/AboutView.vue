6d240b23e3c94feebecdfd49e2257db49443d397
lewisssgrtr <158985477+lewisssgrtr@users.noreply.github.com>
2024-03-11 17:29:51 +0800

    Initial push

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Test page (To add data)</h1>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,138 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import router from '@/router';
+import axios from 'axios';
+
+export default{
+data(){
+  return {
+    employees: [],
+    employee_details: null
+  }
+},
+async mounted() {
+  await axios.get("http://localhost:8000/api/employee").then((response) =>{
+    console.log(response.data.data);
+    console.log(response.status);
+    this.employees = response.data.data;
+  }).catch((error) => {
+
+  });
+},
+methods: {
+  async onShowEmployeeDetails(id){
+      var response = await axios.get(`http://localhost:8000/api/employee/${id}`);
+      this.employee_details = response.data.data;
+  },
+ async onCreateEmployee(){
+      var response = await axios.post(`http://localhost:8000/api/employee`);
+  }
+},
+}
 </script>
 
 <template>
-  <main>
-  </main>
+  <table class="table table-dark table table-hover">
+    <thead class="text-center">
+        <tr>
+          <th scope="col">NAME</th>
+          <th scope="col">EMPLOYEE ID</th>
+          <th scope="col">GENDER</th>
+          <th scope="col">BIRTHDAY</th>
+        </tr>
+    </thead>
+
+    <tbody class="text-center">
+      <tr  v-for="element of employees" :key="element" @click="$router.push({name: 'employee.details', params: {id: element.id} })">
+        <td class="table-light">{{element.name}}</td>
+        <td class="table-light">{{element.id}}</td>
+        <td class="table-light">{{element.gender}}</td>
+        <td class="table-light">{{element.birthday}}</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <div>
+    <button type="button" class="btn btn-primary mx-5" @click="$router.push('/about')">Add</button>
+  </div>
+  
+
+  <!-- <div>
+    <ol>
+      <li v-for="element of employees" :key="element" @click="onShowEmployeeDetails(element.id)">Employee Name:{{element.name}}</li>
+    </ol>
+  </div> -->
+<!--       
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+
+  <RouterView />
 </template>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
